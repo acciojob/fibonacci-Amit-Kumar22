04f9c86190d6b77a48ccc0f7a3b6f65488793c6e
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-07-28 14:27:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 
 const number = parseInt(prompt('Enter the number of terms: '));
 let n1 = 0, n2 = 1, nextTerm;
-
-console.log('Fibonacci Series:');
-
-for (let i = 1; i <= number; i++) {
-    console.log(n1);
+for (let i = 0; i <= number; i++) {
     nextTerm = n1 + n2;
     n1 = n2;
     n2 = nextTerm;
+	console.log(nextTerm);
 }
