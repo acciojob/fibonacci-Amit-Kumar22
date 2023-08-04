140417a469ebd2558c2cbcd5e50b20d17c3ba822
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-08-04 20:26:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-const number = parseInt(prompt('Enter the number of terms: '));
+//const number = parseInt(prompt('Enter the number of terms: '));
 let n1 = 0, n2 = 1, nextTerm;
 for (let i = 0; i <= number; i++) {
     nextTerm = n1 + n2;
     n1 = n2;
     n2 = nextTerm;
-	console.log(nextTerm);
 }
+console.log(nextTerm);
